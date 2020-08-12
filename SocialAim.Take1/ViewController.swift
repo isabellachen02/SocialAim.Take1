620a2c63b6d4f4008a33dfdfb2bef62331e23f33
Isabella Chen <chenisabella02@gmail.com>
2020-08-12 09:20:42 -0700

    Initial Commit

---
+++ SocialAim.Take1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SocialAim.Take1
+//
+//  Created by Bella on 8/12/20.
+//  Copyright © 2020 Isabella Chen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
